f6aff5874e47d616951edf1e520efb743db821cc
lino_76 <lino_76@hotmail.com>
2016-09-23 23:12:38 -0400

    adding client file

---
+++ build/client.py
@@ -0,0 +1,16 @@
+﻿import sys
+
+def main(iParameters):
+    print('iParameters:',iParameters)
+
+    try:
+        # Raise an exception with argument
+        if not iParameters:
+            raise Exception('Empty Parameter List')
+    except Exception as error:
+        # Catch exception
+        print('Error: ', error)
+  
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
