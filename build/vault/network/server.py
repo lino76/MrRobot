e80274064f5a772f6ee4a9a3240e960cb5b8a3d6
Morozov-Sergei <Rilyntaar@gmail.com>
2016-09-25 21:14:34 +0300

    updated server

--- build/vault/network/server.py
+++ build/vault/network/server.py
@@ -5,35 +5,34 @@ import json
 def handle_client(client_socket):
     # TODO: Add actual responses
 
-    while True:
-        data = b''
-        while not b'***' in data:
-            tmp  = client_socket.recv(1024)
-            if not tmp:
-                break
-            data += tmp
-            print("[*] Received in socket", data)
+    client_socket.settimeout(30)
+    data = b''
+    while not b'***' in data:
+        tmp  = client_socket.recv(1024)
+        if not tmp:
+            break
+        data += tmp
+        print("[*] Received in socket", data)
 
             #TODO: It could issue the TIMEOUT status if input  with *** is not received within 30 seconds
-        if data and b'***' in data:
-            udata = data.decode()
-            command = udata.split('***', 1)[0]
-            print("[*] Received: \n%s" % command)
+    if data and b'***' in data:
+        udata = data.decode()
+        command = udata.split('***', 1)[0]
+        print("[*] Received: \n%s" % command)
 
-            # Parse and process command
-            try:
-                #parser = Parser(command)
-                # pass return the return value
-                client_socket.send("good job slick".encode())
-            except Exception as e:
-                #TODO Catch security exceptions
-                # send response
-                client_socket.send("{exception}".encode())
-        else:
-            #TODO: Send time-out
-            break
-        client_socket.close()
-
+        # Parse and process command
+        try:
+            #parser = Parser(command)
+            # pass return the return value
+            client_socket.send("good job slick".encode())
+            print("[*] Sended")
+        except Exception as e:
+            #TODO Catch security exceptions
+            # send response
+            client_socket.send("{exception}".encode())
+    else:
+        #TODO: Send time-out
+        pass
 
 
 
@@ -56,16 +55,15 @@ def start(port, password):
     while True:
         (client_socket, address) = server.accept()
         print("[*] Accepted connection from: %s:%d" % (address[0], address[1]))
-        client_socket.settimeout(30)
+        #client_socket.settimeout(30)
         try:
             handle_client(client_socket)
 
             # client_handler = threading.Thread(target=handle_client, args=(client_socket,))
             # client_handler.start()
 
-        except :
-            #TODO: handle time out
-            #TODO: Programm FAILED: the only status code sent back to the client is FAILED
+        except Exception as e :
+            print("FAILED", e)
             client_socket.send(json.loads({"status":"FAILED"}))
-
-    client_socket.close()
+        client_socket.close()
+    socket.close()
