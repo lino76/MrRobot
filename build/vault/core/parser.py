1bed75f66787f2d9526f871cc3848774bf1cc0b3
jenarvaezg <jenarvaezg@gmail.com>
2016-09-27 16:23:25 +0200

    Added more funcs, need to finish some stuff

--- build/vault/core/parser.py
+++ build/vault/core/parser.py
@@ -1,5 +1,5 @@
 import re
-import vault.program
+from vault.core.program import *
 from vault.error import VaultError
 '''Parse the program'''
 
@@ -24,33 +24,133 @@ class Parser:
 	#maybe move this outside or make a static func
 	def validate_string_constant(self, string):
 		string = self.dequote(string)
-		return re.fullmatch('[A-Za-z0-9_ ,;\.?!-]*', string) and len(string) < 65536
+		return re.fullmatch('[A-Za-z0-9_ ,;\.?!-]*', string) and  \
+		   len(string) < 65536
 
 	def validate_identifier(self, string):
-		return string not in self.reserved_words and re.fullmatch('[A-Za-z][A-Za-z0-9_ ]*', string) and len(string) < 256
+		return string not in self.reserved_words and \
+		   re.fullmatch('[A-Za-z][A-Za-z0-9_ ]*', string) and len(string) < 256
+
+	def validate_comment(self, string):
+		return re.fullmatch(
+			'[\/][\/][A-Za-z][A-Za-z0-9_ ]*$', string)
+
+	def remove_comments(self, string):
+
+		if string.startswith("//"):
+			if not self.validate_comment(string):
+				raise VaultError(1, "Comment not valid: " + string)
+			else:
+				return ""	
+		if "//" in string:
+			uncommented = string.split("//")[0]
+			comment = string[len(uncommented) - 1: ]
+			if not self.validate_comment(comment):
+				raise VaultError(1, "Comment not valid: " + string)
+			else:
+				return uncommented
+		return string
+			
+
+	def parse_value(self, string):
+		#values can be either identifiers, record.field or string literals
+
+		#test for string literal
+		if '"' in string:
+			if not (s.startswith('"') and s.endswith('"')):
+				raise VaultError(1, "Value not valid, quotes misplaced ?" + string)
+			if not self.validate_string_constant(string):
+				raise VaultError(1, "String literal not matching re: " + string)
+			return string #TODO CHANGE SO THIS RETURNS AN SPECIAL TYPE
+		#test for record.field
+		if "." in string:
+			splitted = string.split(".")
+			if len(splitted) != 2:
+				raise VaultError(1, "Record contains wrong number of dots (.): " + string)
+			if not self.validate_identifier(splitted[0]) or not self.validate_identifier(splitted[1]):
+				raise VaultError(1, "One of the record fields are not valid: " + string)
+			return string #TODO CHANGE SO THIS RETURNS AN SPECIAL TYPE
+
+		#now only value posible
+		if not self.validate_identifier(string):
+			raise VaultError(1, "Single identifier not valid: " + string)
+		return string #TODO CHANGE SO THIS RETURNS AN SPECIAL TYPE
+
 
 	def parse_expression(self, splitted):
-		if 
+		#an expression is either a value, an empty list or a fieldval
+		s = ''.join(splitted)
+		#test for empty list
+		if s == "[]": 
+			return Expression("list", [])
+
+		#test for record
+
+		if s.startswith("{") and s.endswith("}"):
+			try:
+				content = {}
+				fieldvals = s[1:-1].split(",") # remove braces and split fieldvals
+				for fieldval in fieldvals:
+					fieldval = fieldval.split("=")
+					if len(fieldval) != 2:
+						raise VaultError(1, "Fieldval had more than one equal sign: " + s)
+					identifier = fieldval[0]
+					value = selfparse_value(fieldval[1])
+					content['id'] = value
+				return Expression('record', content)
+			except Exception as e:
+				if type(e) is VaultError:
+					raise e
+				raise VaultError(1, "Malformed expression: " + s)
+
+		#Now it can only be a value		
+		value = self.parse_value(s)
+		return Expression('value', value)
 
 
+		
+	def parse_create_principal(self, splitted):
+		expressions = {}
+		if len(splitted) != 2: #only 2 args expected
+			raise VaultError(1, "create principal got more than 2 args: " + splitted)
+		#arg 0 must be a identifier, it is the principal we want to create and it must be a valid identifier
+		if not self.validate_identifier(splitted[0]):
+			raise VaultError(1, "create principal got a invalid identifier: " + splitted)
+		expressions['principal_to_create'] = splitted[0]
+
+		#arg 1 must be a string, it's the password we want to set for the principal
+		if not self.validate_string_constant(splitted[1]):
+			raise VaultError(1, "create principal got a invalid string: " + splitted)
+		expressions['password_to_set'] = self.dequote(splitted[1])
+		return expressions
 
 	def parse_command(self, line):
-		line = line.split(" ")
+		line = line.strip().split(" ")
+		while '' in line: 
+			line.remove('')
 		if line[0] == "exit":
 			if len(line) != 1:
 				raise VaultError(1, "Error parsing prog: exit should have no args")
-			return program.Command('exit')
+			return Command('exit')
+
 		if line[0] == "return":
-			expressions = {'return_value': parse_expression(line[1:])}
-			return program.Command('return', expressions)
+			expressions = {'return_value': self.parse_expression(line[1:])}
+			print(expressions)
+			return Command('return', expressions)
+
 		if line[0] == "create" and line[1] == "principal":
-			expressions = parse_create_principal(line[2:])
-			return program.Command('create_principal', expressions)
+			expressions = self.parse_create_principal(line[2:])
+			return Command('create_principal', expressions)
+
+		#TODO
 		return line[0]
 
 	def parse_prog(self, line):
 		#I'm thinking about doing this slowly and carefully, but we could just cram all the inequalities together
-		line = line.split(" ")
+
+		line = line.strip().split(" ") #remove spaces at beginning and end, then split
+		while '' in line: 
+			line.remove('') #remove empty entries in list due to multiple spaces together
 		if line[0] != "as" and line[1] != "principal":
 			raise VaultError(1, "Error parsing prog")
 		if not self.validate_identifier(line[2]):
@@ -65,11 +165,22 @@ class Parser:
 
 
 	def parse(self, program):
-		lines = program.src.split("\n")
+		lines_tmp = program.src.split("\n")
+		lines = []
+		for line in lines_tmp:
+			line = self.remove_comments(line)
+			if line != "":
+				lines.append(line)
+
 		principal, password = self.parse_prog(lines.pop(0))
 		program.principal = principal
 		program.password = password
 		program.commands = []
+
 		for line in lines:
 			program.commands.append(self.parse_command(line))
+
+		for command in program.commands:
+			print(str(command))
+
 		return program
--- build/vault/core/program.py
+++ build/vault/core/program.py
@@ -1,20 +1,30 @@
 '''Represents each of the expressions that can appear in a command'''
 class Expression:
 
-    def __init__(self, type, content):
-        self.type = type #'value', 'list' (or '[]'), 'record'
+    def __init__(self, expr_type, content):
+        self.expr_type = expr_type #'value', 'list' (or '[]'), 'record'
         self.content = content
+
+    def __str__(self):
+        print("self")
+        print("HEY, content: " + self.content)
+        return self.expr_type + ": " + str(self.content)
+
+    def __repr__(self):
+        print("repr")
+        return str(self)
+
 
     def get(self, field=None):
         if self.type == 'value':
-            return self.content
+            return self.content #maybe this is not that simple, since values can either be literals or have and identifier
         if self.type == 'list':
             return content[int(field)]
         if self.type == 'record':
             return content[str(field)]
 
 
-    def set(self, value field=None):
+    def set(self, value, field=None):
         if self.type == 'value':
             self.content = value
         elif self.type == 'list':
@@ -36,8 +46,19 @@ class Expression:
 class Command:
 
     def __init__(self, name, expressions = {}):
-        self.name = name #This would be something like 'exit', 'return', 'foreach'...
+        self.name = name #This would be something like 'exit', 'return', 'foreach'... or NOP (?)
         self.expressions = expressions #This would be a dict of expressions {'return_value': 'x'}, {'create_principal_name': 'bob', 'create_principal_pass': 'password'}
+
+    def __str__(self):
+        s = self.name + "("
+        for exp_k in self.expressions.keys():   
+            s += exp_k + ": " + str(self.expressions[exp_k]) + ", "
+        s = s[:-2] + ")" #remove comma and space and add parenthesis
+        return s
+
+    def __repr__(self):
+        return str(self)
+
 
 '''Represents the program to be executed'''
 
