641d362b4849ac5779be3aeed5db6e0a60a3d464
Jason <jake1164@hotmail.com>
2016-10-05 09:39:40 -0400

    Added input to allow you to select a team, all or repeat last test

--- breakit/readme.md
+++ breakit/readme.md
@@ -9,11 +9,17 @@ teams\team1\build
 teams\team2\build
 teams\team3\build
 
+To stop processing on a team folder and exclude it place a .ignore in the root of that teams folder.
+
 Break scripts are placed in a subdirectory with the default subdirectory being 'break_scripts'. 
 
 Command Line Arguments
 -p default 1024
 -s break it script folder. default 'break_scripts'
--r  Rebuild all projects
--t  Path to team folders. default 'teams'
+-r Rebuild all projects
+-s folder to use holding teams source code. Note this must be the parent folder of a team folder.
+ 
+######  Command Line Execution ######
+-m Manual Program name. If used this is executed from the command line and exits when finished. 
+-t Teams to execute test against, only used in manual mode for executing tests from the command line. 
 
--- breakit/test_runner.py
+++ breakit/test_runner.py
@@ -5,93 +5,93 @@ import socket
 import json
 import subprocess
 import time
+import sys
 
 data_path = 'break_scripts'
 teams_root = 'teams'
 port = 1024
 
 class TeamFolders:    
-    __team_path = 'teams' #Path to source code of all teams.    
-    __folders = []
+    teams_root = 'teams' #Path to source code of all teams.    
+    __teams = [] # folders of the teams is also used as the team id.
     __rebuild = False
     __rebuilt = False
     __current = 0
 
-    def __init__(self, force_rebuild, teams_folder = None, target_teams = None):
+    def __init__(self, force_rebuild, teams_folder = None):
         if teams_folder != None:
             self.__team_path = teams_folder
         
-        self.__root = os.path.join(os.path.dirname(__file__), self.__team_path)
+        self.teams_root = os.path.join(os.path.dirname(__file__), self.teams_root)
         
         #check if a build has been executed already
-        if force_rebuild or not os.path.isfile(os.path.join(self.__root, '.built')):
+        if force_rebuild or not os.path.isfile(os.path.join(self.teams_root, '.built')):
             self.__rebuild = True
         
         #for folder in os.listdir(self.__team_path):
-        for folder in os.listdir(self.__root):
-            target_team = os.path.join(self.__root, folder)
+        for team in os.listdir(self.teams_root):
+            team_folder = os.path.join(self.teams_root, team)
             # if Folder contains .ignore skip the folder.
-            if not os.path.isdir(target_team):
+            if not os.path.isdir(team_folder):
                 continue
 
             try:
-                if not os.path.isfile(os.path.join(target_team, '.ignore')):
-                    self.__folders.append(target_team)
+                if not os.path.isfile(os.path.join(team_folder, '.ignore')):
+                    # store the teamid and path to folder in dictionary.
+                    self.__teams.append(team)
                     if self.__rebuild:
-                        print('Building team #: ', folder)
-                        self.__build(target_team, True)
+                        print('Building team #: ', team)
+                        self.__build(team, True)
             except Exception as e:
                 print(e)
         
         if self.__rebuild:
             self.__rebuilt = True # Flag to avoid double rebuilding the first time.
-            file = os.path.join(self.__root, '.built')
+            file = os.path.join(self.teams_root, '.built')
             with open(file, 'w') as f: pass 
 
     def __iter__(self):
-        return iter(self.__folders)            
+        return iter(self.__teams)            
 
     def build_all(self):
         if self.__rebuilt:
             return
         for folder in self.__folders:
-            __build(folder, True)
+            __build(team, True)
 
-    def __build(self, folder, force = False):
-        if force or not os.path.isfile(os.path.join(self.__root, folder, 'build', 'server')):
+    def __build(self, team, force = False):
+        build_folder = (os.path.join(self.teams_root, team, 'build'))
+        if force or not os.path.isfile(os.path.join(build_folder, 'server')):
             try:
-                make = os.path.join(self.__root, folder, 'build')
-                ret = subprocess.Popen(['make'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=make)                
+                ret = subprocess.Popen(['make'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=build_folder)                
                 ret.wait()               
-                if ret.returncode == 2:
-                    print(ret.stdout.readlines())
-                    
+                if ret.returncode == 2: # return code 2 is an error, print the standard out.
+                    print(ret.stdout.readlines())                    
                     print(ret.returncode)
             except Exception as e:
-                print('Exception raised building {} : '.format(folder), e)
+                print('Exception raised building {} : '.format(team), e)
 
 class Server:
     host = ''
     port = 1024
 
     def __init__(self, build_folder):
-
         self.server = os.path.join(build_folder, 'build', 'server')       
         if not os.path.isfile(self.server):            
-            raise
+            raise Exception('Server not found')
 
-    def runServer(self, port):
+    def start_server(self, port):
         self.port = port
-        self.proc = subprocess.Popen( [self.server, str(self.port)])
+        self.proc = subprocess.Popen([self.server, str(self.port)])
         time.sleep(2)
 
         self.proc.poll()
 	    # Port already busy, try next port
         if self.proc.returncode == 63:
-            return self.runServer( port + 1)
+            return self.start_server( port + 1)
         return self.port
 
-    def stopServer(self):
+    def stop_server(self):
         self.proc.terminate()
         self.proc.wait()
         print( "server exited with return code: " + str(self.proc.returncode))
@@ -180,27 +180,33 @@ class Client:
         else:
             print('File does not exist: ', testfile)
 
-
-def send(teams, program):
+def send(teams, team_list, program):
     for team in teams:
-        try:
-            server = Server(team)
-            dicare = server.runServer(port)
-            
-            client = Client()
-        except Exception as e:
-            print(e)
+        # only execute the tests on the specified teams.
+        if team_list[0] == 'all' or team in team_list:
+            try:
+                server = Server(os.path.join(teams.teams_root, team))
+                server.start_server(port)
+                
+                client = Client()
+            except Exception as e:
+                print(e)
+            finally:
+                if server is not None:
+                    server.stop_server()
 
 if __name__ == '__main__':
+    print(sys.version)
     # Parse the command lines.  Expect a port followed by a data folder path
     cmd_parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter)
-    cmd_parser.add_argument('-p', type=int, dest="port", default=1024)
-    cmd_parser.add_argument('-d', type=str, dest="data_path", default=data_path, required=False)
+    cmd_parser.add_argument('-p', type=int, dest="port", default=1024)        
+    cmd_parser.add_argument('-s', '--source', type=str, dest='team_path', required=False, help='Path to folder containing the folders of each teams code.' )
+    cmd_parser.add_argument('-d', type=str, dest="data_path", default=data_path, required=False)        
+    cmd_parser.add_argument('-r', '--rebuild', action='store_true', help='Force all known team projects to rebuild.' )
+    # These options are to run a test from the command line, -t is ignored without a -m but is optional, if not include all teams are run
+    # Note these are not implemented yet.
     cmd_parser.add_argument('-m', type=str, dest="manualprogram", required=False)
     cmd_parser.add_argument('-t', '--teams', type=str, help='List of teams separated by commas', required=False)
-    cmd_parser.add_argument('-r', '--rebuild', action='store_true', help='Force all known team projects to rebuild.' )
-    cmd_parser.add_argument('-s', '--source', type=str, dest='team_path', required=False, help='Path to folder containing the folders of each teams code.' )
-
 
     args = cmd_parser.parse_args()
 
@@ -216,10 +222,9 @@ if __name__ == '__main__':
     
     # Get all the team folders and compile if required.    
     # TODO: Implement target teams to limit the execution to a few teams.
-    teams = TeamFolders(args.rebuild, team_path, target_teams)
+    teams = TeamFolders(args.rebuild, team_path)
     if args.rebuild:
         teams.build_all()
-
 
     # If a manual program then execute a single script   
 #    if manualprogram:
@@ -234,15 +239,27 @@ if __name__ == '__main__':
 #        clientSend(manualprogram)
     else:
         print('test file mode..')
+        test_file = None
         while True:
-            select = input('Enter File Name or type exit:')
-            if select == 'exit':
+            print('available teams:\n', str.join(', ', teams))
+            #t_input = input(' \n## Press Enter to run last team(s) / program selection ##\n or type exit:')
+            t_input = input('Enter team or teams (separated by comma), enter for all or type "r" to repeat: ')
+            
+            if t_input == '':
+                t_input = 'all'
+            if t_input != 'r' or test_file is None:
+                # Return will shortcut rerun last selected test / teams
+                team_list = t_input.split(',')
+                test_file = input('Enter File Name or type exit:')                
+
+            if test_file == 'exit':
                 exit()
-            print(select)
-            test_file = os.path.join(os.path.dirname(__file__), data_path, select)
+            #print(select)
+            
+            test_file = os.path.join(os.path.dirname(__file__), data_path, test_file)
             if not test_file.endswith(".json"):
                 test_file += ".json"
-            send(teams, test_file)
+            send(teams, team_list, test_file)
 
 
 
